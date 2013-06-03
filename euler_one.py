4710bcc4f7271385c2185f8f66d45d6407edc7f3
cponeill <caseyoneill78@hotmail.com>
2013-06-03 12:14:18 -0600

    Update euler_one.py

--- euler_one.py
+++ euler_one.py
@@ -1,4 +1,4 @@
-# This is my answer to question number one. Pretty simple enough
+# This is my answer to question number one. Pretty simple enough.
 
 def euler_one(s):
     return sum(n for n in range(s) if n%3 == 0 or n%5 == 0)
