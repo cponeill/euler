f40133370d815801fdf705c45869640954bdc7d9
cponeill <caseyoneill78@hotmail.com>
2013-06-03 12:14:28 -0600

    Update euler_one.py

--- euler_one.py
+++ euler_one.py
@@ -3,5 +3,5 @@
 def euler_one(s):
     return sum(n for n in range(s) if n%3 == 0 or n%5 == 0)
 
-# This will print out the answer you need for the very first question which is 233168
+# This will print out the answer you need for the very first question which is 233168.
 print euler(1000)
