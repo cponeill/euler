5d652f7fc9e7349b2f076fd9692d44e2a07df562
cponeill <caseyoneill78@hotmail.com>
2013-06-14 15:08:22 -0600

    Create euler_two.py

---
+++ euler_two.py
@@ -0,0 +1,25 @@
+begin=1
+end=100
+F=[]
+Even=[]
+
+def even(x):
+        if (x % 2 == 0):
+                return x
+
+def answer():
+        for y in range(begin,end):
+                if i > 2:
+                        y=F[i-2]+FSeq[i-3]
+                        F.append(brev)
+                else:
+                        F.append(i)
+
+
+        for l in FS:
+                if even(l) and l < 4000000:
+                        Even.append(l)
+        print sum(Even)
+
+
+solution()
